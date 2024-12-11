a49683f005a349470f350b54961304d23a266998
Zenitsu58494 <Zenitsu55555555@gmail.com>
2024-12-12 03:25:46 +0800

    Third

--- src/app/Table.jsx
+++ src/app/Table.jsx
@@ -1,24 +1,76 @@
 import Image from "next/image";
+import { CiHome } from "react-icons/ci";
+import { CiLocationOn,CiHeart,CiUser } from "react-icons/ci";
 export default function Table(props) {
+  let weatherImage="/sun.png"
+  if(props.status==="Bright"){
+    weatherImage="/sun.png"
+  } else if(props.status==="Cloudy" && props.style==="mt-60 bg-black"){
+    weatherImage="/NightClouds.png"
+
+  }
+  else if(props.status==="Rainy" && props.style==="mt-60 bg-black"){
+    weatherImage="/NightRain.png"
+
+  }
+  else if(props.status==="Stormy" && props.style==="mt-60 bg-black"){
+    weatherImage="/NightStorm.png"
+
+  }
+  else if(props.status==="Windy" && props.style==="mt-60 bg-black"){
+    weatherImage="/NightWind.png"
+
+  }
+  
+  else if(props.status==="Clear" && props.style==="mt-60 bg-black"){
+    weatherImage="/Moon.png"
+  } else if(props.status==="Cloudy"){
+    weatherImage="/Clouds.png"
+
+  }
+  else if(props.status==="Snowy"){
+    weatherImage="/DaySnow.png"
+  }
+  else if(props.status==="Stormy"){
+    weatherImage="/DayStorm.png"
+  }
+  else if(props.status==="Windy"){
+    weatherImage="/DayWind.png"
+
+  }
+
+
+
+
+
+
+  
+ 
   return (
     <>
-      <div className="w-[400px] mt-40 ml-[300px] bg-white h-[800px] rounded-3xl">
+      <div className={`w-[400px]  ml-[400px] h-[900px] rounded-3xl ${props.style}`}>
         <div className="ml-10 pt-[50px]">
-          <h1>{props.date}</h1>
-          <h2 className="text-6xl">{props.city}</h2>
+          <h1 className="text-gray-400">{props.date}</h1>
+          <h2 className="text-6xl text-gray-400">{props.city}</h2>
         </div>
 
         <Image
           className="ml-20 mt-[130px]"
-          src={props.image}
+          src={weatherImage}
           width={260}
           height={260}
           alt=""
         />
         <div>
-          <div className="text-[144px] ml-[50px]">{props.celsius}</div>
+          <div className="text-[144px] ml-[50px] text-gray-400">{props.celsius}</div>
           <a className="text-[24px] ml-[50px] text-[#FF8E27]">{props.status}</a>
         </div>
+       <div className="flex gap-[75px] ] ml-[50px] pt-10">
+        <CiHome  className=" text-gray-400 size-6"/>
+        <CiLocationOn  className=" text-gray-400 size-6"/>
+        <CiHeart  className= "text-gray-400 size-6"/>
+        <CiUser  className="text-gray-400 size-6"/>
+       </div>
       </div>
     </>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,32 +3,37 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { IoIosSearch } from "react-icons/io";
 import Table from "./Table";
+import { CiHome,CiLocationOn,CiHeart,CiUser } from "react-icons/ci";
+
 
 export default function Home() {
   return (
     <>
-      <div className=" w-full h-screen flex">
+      <div className=" w=full h-screen flex">
         <div className="bg-[#F3F4F6] w-[50%] ">
-          <div className="bg-white w-[570px] rounded-[30px] h-20 ml-12 flex justify-center text-center gap-4">
+          <div className="bg-white w-[570px] rounded-[30px] h-20 ml-[200px] flex justify-center text-center gap-4">
             <IoIosSearch className="w-9 h-9 mt-5 " />
             <input className=" w-[450px] h-10 mt-5" placeholder="Search" />
           </div>
           <Table
+          style={"mt-40 bg-white"}
             celsius={26}
-            status={"Bright"}
+            status={"Snowy"}
             city={"Ulaanbator"}
             date={"September 10, Monday"}
-            image={"/sun.png"}
+          
+           
           />
         </div>
-        <div className="bg-black w-[50%]">
+        <div className=" w-[50%] bg-[url('/Subtract.png')] bg-no-repeat bg-[length:100%] ">
           <Table
+          style={"mt-60 bg-black"}
             className="mt-60"
-            celsius={12}
-            status={"Clear"}
+            celsius={9}
+            status={"Stormy"}
             city={"Ulaanbator"}
             date={"September 10, Monday"}
-            image={"/Moon.png"}
+          
           />
         </div>
       </div>
