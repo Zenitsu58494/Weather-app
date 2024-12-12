60a77e971818a436e2039ab536873cc2cf423b14
24LP3531 24LP3531 <24LP3531@setups-iMac-3.local>
2024-12-12 18:32:24 +0800

    fifth

--- src/app/Status.jsx
+++ src/app/Status.jsx
@@ -1,42 +1,23 @@
-
-    let weatherImage="/sun.png"
-    if(props.status==="Bright"){
-      weatherImage="/sun.png"
-    }else if(props.status==="Clear" && props.style==="mt-60 bg-black"){
-      weatherImage="/Moon.png"
-    } else if(props.status==="Cloudy"){
-      weatherImage="/Clouds.png"
-  
-    }
-    else if(props.status==="Snowy"){
-      weatherImage="/DaySnow.png"
-    }
-    else if(props.status==="Stormy"){
-      weatherImage="/DayStorm.png"
-    }
-    else if(props.status==="Windy"){
-      weatherImage="/DayWind.png"
-  
-    }
-    else if(props.status==="Cloudy" && props.style==="mt-60 bg-black"){
-      weatherImage="/NightClouds.png"
-  
-    }
-    else if(props.status==="Rainy" && props.style==="mt-60 bg-black"){
-      weatherImage="/NightRain.png"
-  
-    }
-    else if(props.status==="Stormy" && props.style==="mt-60 bg-black"){
-      weatherImage="/NightStorm.png"
-  
-    }
-    else if(props.status==="Windy" && props.style==="mt-60 bg-black"){
-      weatherImage="/NightWind.png"
-  
-    }
-    return(
-        <>
-        {weatherImage}
-        </>
-    )
+let weatherImage = "/sun.png";
+if (props.status === "Bright") {
+  weatherImage = "/sun.png";
+} else if (props.status === "Clear" && props.style === "mt-60 bg-black") {
+  weatherImage = "/Moon.png";
+} else if (props.status === "Cloudy") {
+  weatherImage = "/Clouds.png";
+} else if (props.status === "Snowy") {
+  weatherImage = "/DaySnow.png";
+} else if (props.status === "Stormy") {
+  weatherImage = "/DayStorm.png";
+} else if (props.status === "Windy") {
+  weatherImage = "/DayWind.png";
+} else if (props.status === "Cloudy" && props.style === "mt-60 bg-black") {
+  weatherImage = "/NightClouds.png";
+} else if (props.status === "Rainy" && props.style === "mt-60 bg-black") {
+  weatherImage = "/NightRain.png";
+} else if (props.status === "Stormy" && props.style === "mt-60 bg-black") {
+  weatherImage = "/NightStorm.png";
+} else if (props.status === "Windy" && props.style === "mt-60 bg-black") {
+  weatherImage = "/NightWind.png";
 }
+return <>{weatherImage}</>;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,38 +3,50 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { IoIosSearch } from "react-icons/io";
 import Table from "./Table";
-
-
+import { Corners } from "./components/Corners";
 
 export default function Home() {
+  const [change, Setchange] = useState("Bright");
+  const [search, Setsearch] = useState("");
   return (
     <>
       <div className=" w=full h-screen flex">
         <div className="bg-[#F3F4F6] w-[50%] ">
           <div className="bg-white w-[570px] rounded-[30px] h-20 ml-[200px] flex justify-center text-center gap-4">
             <IoIosSearch className="w-9 h-9 mt-5 " />
-            <input className=" w-[450px] h-10 mt-5" placeholder="Search" />
+            <input
+              onChange={(e) => {
+                Setsearch(e.target.value);
+                console.log(search);
+              }}
+              className=" w-[450px] h-10 mt-5 outline-none"
+              placeholder="Search"
+            />
+            <button
+              onClick={() => {
+                Setchange(search);
+              }}
+              className="w-7 h-7 absolute z-30 bg-black"
+            ></button>
           </div>
           <Table
-          style={"mt-40 bg-white"}
+            style={"mt-40 bg-white"}
             celsius={26}
-            status={"Snowy"}
+            status={change}
+            d
             city={"Ulaanbator"}
             date={"September 10, Monday"}
-          
-           
           />
         </div>
-        <div className=" w-[50%] bg-[url('/Subtract.png')] bg-no-repeat bg-[length:100%] ">
+        <div className=" w-[50%] bg-[#0F141E] relative bg-no-repeat bg-[length:100%]  ">
           <Table
-          style={"mt-60 bg-black"}
-            className="mt-60"
+            style={"mt-60 bg-black"}
             celsius={9}
-            status={"Stormy"}
+            status={change}
             city={"Ulaanbator"}
             date={"September 10, Monday"}
-          
           />
+          <Corners />
         </div>
       </div>
     </>
